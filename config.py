85ac35d146564b965606de4e1c56ae66f0d2e040
GSDion <97761993+GSDion@users.noreply.github.com>
2024-09-20 14:48:41 -0400

    Update gitignore
    - Update gitignore file
    Remove c*****

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = "b's}\x05\xfa\xf6\xeb\xcc\x13\xd9\x17d:'"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
