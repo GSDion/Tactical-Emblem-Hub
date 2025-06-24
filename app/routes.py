c825d037173c6189e7b8c9237e66500f92bbd475
GSDion <97761993+GSDion@users.noreply.github.com>
2025-06-24 03:58:04 -0400

    altered links/routes in index.html
    - added routes for index.html anchors
    - created faq.html
    - altered hrefs for navbar links (dont currently work) in index.html
    - altered hrefs for footer in index.html
    - removed remnants of fullpage.js in index.html
    - added links to signup and login pages to buttons in index.html
    - changed "about_page_title" from class to id in index.html
    - added basic layout to signup.html

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
 from flask import render_template, current_app as app
+from flask import redirect, url_for
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# SIGNUP
+
+# INDEX: title_section
+@app.route('/hero')
+def title_section():
+    return redirect(url_for('index',_anchor='title_page'))
+
+# INDEX: about_section
+@app.route('/about')
+def about_section():
+    return redirect(url_for('index',_anchor='about_page_title'))
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
@@ -13,6 +24,14 @@ def signup():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 # CREATE TEAM
 
